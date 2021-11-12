cd92be61c5ea55a73dd9bde1c3268f7a1347aa6f
Fernando Cortes <53202695+Ferloxc@users.noreply.github.com>
2021-11-11 22:46:06 -0600

    Delete db365BeautySalon.sql

--- database/db365BeautySalon.sql
+++
@@ -1,9 +0,0 @@
-create database dbBeautySalon;
-use dbBeautySalon;
-
-create table usuarios(
-    id int not null,
-    nombre_usuario varchar(20) not null,
-    password varchar(60) not null,
-    nombre_completo varchar(100) not null
-)
