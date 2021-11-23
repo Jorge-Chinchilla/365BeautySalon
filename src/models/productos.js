328ef8f4a7ba73b5efa8b2697645d5d4bdd7bedb
Kanakku24 <44948710+Kanakku24@users.noreply.github.com>
2021-11-22 22:58:24 -0600

    creada ruta de controladores

--- src/models/productos.js
+++ src/models/productos.js
@@ -1,14 +1,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const ProductoSchema = new Schema({
+const ProductoSchema = new Schema({/*
     _id: { type: String, required: true },
     nombre: { type: String, required: true },
     cantidad: { type: Number, required: true },
     precio_compra: { type: Number, required: true },
     precio_venta: { type: Number, required: true },
     fecha: { type: Date, default: Date.now },
-    categoria: { type: String, required: true }
+    categoria: { type: String, required: true }*/
+
+    id: String,
+    nombre: String,
+    cantidad: Number,
+    precio_compra: Number,
+    precio_venta: Number,
+    fecha: { type: Date, default: Date.now },
+    categoria: String
 });
 
 module.exports = mongoose.model('productos', ProductoSchema);
--- src/routes/producto.js
+++ src/routes/producto.js
@@ -1,36 +1,12 @@
 const express = require('express');
 const router = express.Router();
-
-const Productos = require('../models/productos');
 const { isAuthenticated } = require('../lib/helpers');
+const controllerProducto = require('../controllers/controller.producto')
 
-router.get('/producto', async (req, res) => {
-    const productos = await Productos.find().lean();
-    res.render('menu/producto/producto', { productos });
-});
+router.get('/producto', controllerProducto.getProductos);
+router.get('/producto/add_producto', controllerProducto.getCreateProducto);
+router.get('/producto/info_producto/:id', controllerProducto.getInfoProducto);
 
-router.get('/add_producto', (req, res) => {
-    res.render('menu/producto/add_producto');
-});
-
-router.get('/info_producto/:id', async (req, res) => {
-    const infoProducto = await Productos.findById(req.params.id).lean();
-    console.log(infoProducto);
-    res.render('menu/producto/info_producto', { infoProducto });
-});
-
-router.get('/edit_producto', async (req, res) => {
-    const editProducto = await Productos.findById(req.params.id).lean();
-    console.log(editProducto);
-    res.render('menu/producto/edit_producto', { editProducto });
-});
-
-
-router.post('/add_producto', async (req, res) => {
-    const { _id, nombre, cantidad, precio_compra, precio_venta, categoria } = req.body;
-    const newProducto = new Productos({ _id, nombre, cantidad, precio_compra, precio_venta, categoria });
-    await newProducto.save();
-    res.redirect('/menu/producto');
-});
+router.post('/producto/add_producto', controllerProducto.createProducto);
 
 module.exports = router;
