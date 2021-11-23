9dfa93d9f18d7a1cda57679cef7311b676fd8355
Kanakku24 <44948710+Kanakku24@users.noreply.github.com>
2021-11-23 13:28:07 -0600

    Adicion de cluster

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const { mongodb } = require('./keys')
 
-const database = mongoose.connect(`mongodb://${mongodb.host}:${mongodb.port}/${mongodb.database}`)
+const database = mongoose.connect(`mongodb+srv://admin:asd123@cluster0.pejgq.mongodb.net/beauty_salon_365?retryWrites=true&w=majority`)
 .then((db)=>{
     console.log('Conectado a la BD')
 }).catch((err)=>{
